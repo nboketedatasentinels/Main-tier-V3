5bdf680b4e0c57898d86cb7cf6c4267f203995a2
nboketedatasentinels <nbokete@data-sentinels.com>
2026-03-10 20:31:06 +0100

    Deleted admin-actions.d.ts

--- functions/lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Firebase Cloud Functions Entry Point
- *
- * Exports all Cloud Functions for deployment.
- */
-export * from "./sync-profiles";
-export * from "./automated-peer-matching";
-export * from "./referral-points";
-export * from "./admin-actions";
