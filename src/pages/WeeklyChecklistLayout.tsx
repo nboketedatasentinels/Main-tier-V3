e7573e5f8b91611abaac04ca60d8d82b1ff27baa
Musawu <syntichemusawu645@gmail.com>
2026-08-06 13:53:31 +0100

    Revert "Show correct Done frequencies on the free 4W checklist."
    This reverts commit 4ef38023e8787f6bd16e0d9436f17fdd48b0e212.

--- src/pages/WeeklyChecklistLayout.tsx
+++ src/pages/WeeklyChecklistLayout.tsx
@@ -32,7 +32,6 @@ export const WeeklyChecklistLayout = ({ vm }: { vm: VM }) => {
         pendingWeeksByActivity={vm.pendingWeeksByActivity}
         isWeekLocked={vm.isWeekLocked}
         isAdmin={vm.isAdmin}
-        pinMultiClaimsToStartWeek={vm.journey?.journeyType === '4W'}
         onOpenCurrentWeek={() => vm.setSelectedWeek(vm.journey?.currentWeek ?? vm.selectedWeek)}
         onMarkCompleted={vm.markCompleted}
         onMarkNotStarted={vm.markNotStarted}
